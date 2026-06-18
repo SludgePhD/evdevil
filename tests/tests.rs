09ada74760c79bdbcee29ab39efdfb209c608c2e
Sludge <sludge.phd+git@gmail.com>
2026-06-18 21:58:39 +0200

    test: surface warnings from secondary test runs

--- tests/tests.rs
+++ tests/tests.rs
@@ -6,36 +6,21 @@ use std::{env, ffi::OsString, process::Command};
 
 fn test(args: &[&str]) {
     let cargo = env::var_os("CARGO").unwrap_or_else(|| OsString::from("cargo"));
-    let output = Command::new(cargo)
+    let status = Command::new(cargo)
         .args(&["test", "-p", "evdevil", "--lib"]) // avoid infinite recursion
         .args(args)
-        .output()
+        .status()
         .unwrap();
     assert!(
-        output.status.success(),
-        "cargo exited with error code {:?}. stderr:\n{}\nstdout: {}",
-        output.status.code(),
-        String::from_utf8_lossy(&output.stderr),
-        String::from_utf8_lossy(&output.stdout),
+        status.success(),
+        "cargo exited with error code {:?}",
+        status.code(),
     );
 }
 
-#[test]
-fn no_default_features() {
+fn main() {
     test(&["--no-default-features"]);
-}
-
-#[test]
-fn serde() {
     test(&["--features", "serde"]);
-}
-
-#[test]
-fn tokio() {
     test(&["--features", "tokio"]);
-}
-
-#[test]
-fn async_io() {
     test(&["--features", "async-io"]);
 }
